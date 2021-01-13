cb8015d8176049944ce77eccf1a94ea7c292f9aa
mgriffith-ww <mark.griffith@ww.com>
2021-01-13 15:05:02 -0500

    feat: beginning repo forreact-loadable.

---
+++ src/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+class About extends React.Component {
+
+    render(){
+        return(
+            <section><p>This is an About Component</p></section>
+        )
+    }
+}
+export default About;
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Route , Link} from 'react-router-dom';
+import Loadable from "react-loadable";
+import "./index.css"
+
+const styles = {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    marginTop:50,
+    marginLeft:50
+    
+  };
+  const LoadableAbout = Loadable({
+    loader: () => import('./About'),
+    loading() {
+        return <h1>Loading split out code</h1>
+    }
+  });
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Route exact path="/">
+                    <section style={styles}>
+                        <button><Link to="/about">About Us</Link></button>
+                    </section>
+                </Route>
+                <Route exact path="/about">
+                    <LoadableAbout/>
+                </Route>
+          </Router>
+      );
+    }
+  }
+export default App;
